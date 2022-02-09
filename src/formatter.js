60e28774de10532793107da309d94c33668d4d0f
alla <asheremet@nexstar.tv>
2022-02-08 22:43:15 -0800

    accepting options

--- src/formatter.js
+++ src/formatter.js
@@ -1,18 +1,54 @@
 'use strict';
 
+const colors = {
+  effect: {
+    reset: "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    underscore: "\x1b[4m",
+    blink: "\x1b[5m",
+    reverse: "\x1b[7m",
+    hidden: "\x1b[8m",
+  },
+  text: {
+    black: "\x1b[30m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
+  },
+  background: {
+    black: "\x1b[40m",
+    red: "\x1b[41m",
+    green: "\x1b[42m",
+    yellow: "\x1b[43m",
+    blue: "\x1b[44m",
+    magenta: "\x1b[45m",
+    cyan: "\x1b[46m",
+    white: "\x1b[47m",
+  }
+};
+const config = {
+  pathColor: [colors.effect.dim, colors.text.cyan],
+  exclude: /node_modules/
+}
+
 function filePath() {
   const fileName = __filename.replace(/\//g, '\\/');
+  const root = process.cwd().replace(/\//g, '\\/')
   const trace = new Error().stack.replace(new RegExp(`[\\s\\S]+${fileName}.*?\\)\\s+`, 'm'), '');
-  return trace.split('\n')[0].replace(/\s*at.+\((.*)\)?/, '$1');
+  return trace.split('\n')[0].replace(new RegExp(`\\s*at.+${root}(.*):.*$`), '<root>$1');
 }
 
 function printLine(fn, ...args) {
   const path = filePath();
-  const excludeRE = /node_modules/;
-  if (excludeRE.test(path)) {
+  if (config.exclude.test(path)) {
     fn(...args);
   } else {
-    fn('\x1b[2m', '\x1b[36m', path, '\x1b[0m', ...args);
+    fn(...config.pathColor, path, colors.effect.reset, ...args);
   }
 }
 
@@ -25,4 +61,12 @@ console.trace = printLine.bind(null, console.trace);
 console.timeStamp = console.log.bind(null, new Date());
 console.timeDiff = logTimeDiff;
 
-module.exports = {};
+module.exports = function formatter (options = {}) {
+  Object.assign(config, options);
+  const style = options.style;
+  if(style){
+    config.pathColor = config.pathColor
+      .concat(colors.effect[style.effect], colors.text[style.text], colors.background[style.background])
+      .filter(el => !!el)
+  }
+};
